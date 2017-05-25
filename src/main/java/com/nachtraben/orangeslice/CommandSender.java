9a151844deed2e8a299fa37cea9878feb344c5ca
NachtRaben <stilleraben@gmail.com>
2017-05-25 00:09:23 -0400

    Initial push.

---
+++ src/main/java/com/nachtraben/orangeslice/CommandSender.java
@@ -0,0 +1,15 @@
+package com.nachtraben.orangeslice;
+
+import java.util.concurrent.Future;
+
+/**
+ * Created by NachtDesk on 8/30/2016.
+ */
+public interface CommandSender {
+
+    void sendMessage(String s);
+    boolean hasPermission();
+    String getName();
+
+    Future<CommandEvent> runCommand(String command, String[] args);
+}
